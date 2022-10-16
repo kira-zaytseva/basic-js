b30576f29e7d83fb8f21f5c475098b10a5dee117
Kira Zaytseva <kira.zaytseva97@gmail.com>
2022-10-16 19:51:16 +0300

    solved carbon-dating and dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,10 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if(!Number(sampleActivity) || typeof(sampleActivity) !== 'string' || Number(sampleActivity) <= 0 || Number(sampleActivity) > 15) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))*HALF_LIFE_PERIOD/0.693);
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(members)) {
+    let teamName = members.reduce((acc, rec) => {
+      if (typeof(rec) === 'string') {
+        let nameWithoutSpaces = rec.trim();
+        return acc.concat(nameWithoutSpaces[0].toUpperCase());
+      } else return acc;
+    }, []);
+    return teamName.sort().join('');
+  }
+  return false;
 }
 
 module.exports = {
